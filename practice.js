138ab3e73aa0cefe7590ab02ae18c710b2711a75
Islam Gamal <168297032+islamjamal123@users.noreply.github.com>
2025-04-29 22:35:41 +0300

    practice.js

--- practice.js
+++ practice.js
@@ -38,6 +38,9 @@ function startGame() {
 
     answers = [];
     count = 0;
+    cardsDealt.textContent = 'Cards:';
+    sumCards.textContent = 'Sum:';
+    questions.textContent = 'Do you want to draw a new card?';
     newButton.disabled = false;
     startButton.disabled = true;
 

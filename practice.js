a2400ed86f0e20bc062255241e61e5474c63149a
Islam Gamal <islamjamal1997@gmail.com>
2025-04-29 22:18:39 +0300

    first commit

---
+++ practice.js
@@ -0,0 +1,46 @@
+const cardsDealt = document.querySelector('.cards-dealt');
+const sumCards = document.querySelector('.sum-cards');
+const questions = document.querySelector('.question');
+const newButton = document.querySelector('.new-button');
+const startButton = document.querySelector('.start-button');
+let card = 0;
+let count = 0;
+let answers = [];
+
+function newCard() {
+    
+
+    card = Math.floor(Math.random() * 11) + 1;
+    answers.push(card);
+    cardsDealt.textContent = "Cards: " + answers.join(' ');
+    count++;
+
+    sumCards.textContent = "Sum: " + answers.reduce((a, b) => a + b);
+
+    const sums = answers.reduce((a, b) => a + b);
+
+    if (sums <= 20) {
+        questions.textContent = 'Do you want to draw a new card?';
+        startButton.disabled = true;
+        newButton.disabled = false;
+    } else if (sums === 21) {
+        questions.textContent = "Woohoo! You've got a Blackjack!";
+        startButton.disabled = false;
+        newButton.disabled = true;
+    } else {
+        questions.textContent = "You're out of the game!";
+        newButton.disabled = true;
+        startButton.disabled = false;
+    }
+}
+
+function startGame() {
+
+    answers = [];
+    count = 0;
+    newButton.disabled = false;
+    startButton.disabled = true;
+
+}
+
+
